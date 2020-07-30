44042dcd2ab5e007106d7df6fc32bfe8cb6a7a23
Nimmy Jose <nimmyjose540@gmail.com>
2020-07-30 03:51:06 -0700

    css and js files added

--- bill_frm.php
+++ bill_frm.php
@@ -1,2 +1,28 @@
 
 
+<html>
+    <head>
+     <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Bill Generation</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"/> 
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  
+  
+    </head>
+    <body>
+        <div class="wrapper">
+            <section>
+                <div class="container-fluid">
+                    <div class="jumbotron text-center" style="background-color: #0062cc;color:#FFF; "><h2>Bill Generating System</h2></div></div>
+        
+                
+            </section>
+            </div>
+        </div>
+    </body>
+</html>
